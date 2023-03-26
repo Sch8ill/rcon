0c6c36d0ff1c9492f5ecfa7748b07fa4ef3ecc07
Sch8ill <sch8dill@gmail.com>
2023-03-26 16:35:01 +0200

    add wrong connection state error

--- errors.go
+++ errors.go
@@ -9,4 +9,5 @@ var (
 	errAuthenticationFailed error = errors.New("authentication failed (wrong password?)")
 	errWrongPacketID        error = errors.New("packet send by server has the wrong packet id")
 	errPacketBodyToShort    error = errors.New("body of packet send by server is to short")
+	errWrongConenctionState error = errors.New("operation canceld because of wrong connection state")
 )
